ffa3a3339db2c6e8fa3e3f80ae05367c166825b3
Luki2020 <116828605+Luki2020@users.noreply.github.com>
2022-12-12 20:58:29 +0100

    Update projekt.py

--- projekt.py
+++ projekt.py
@@ -2,15 +2,17 @@ from random import randint, random;
 # wprowadzenie do gry
 print('Witaj w grze ....')
 
-# pobranie imienia
+# wybór imienia bohatera
 imie_boh = ''
 while imie_boh == "":
     imie_boh = input("Wprowadz imie swojego głównego bohatera --> ")
     if imie_boh == "":
         print("Nie wprowadziłes imienia!!!!")
 
-# pobranie typu
+# wybór typu bohatera
 typ_boh = ''
+hp_boh = 0
+kasa_boh = 0
 while typ_boh.upper() not in ['A', 'D', 'Z']:
     print("Twoja kwota w portfelu wynosi 100$ wybierz typ bohatera:")
     typ_boh = input("( Zwykły = z/Z | Atakujący(-40$) = a/A | Defensor(-40$) = d/D ) ---> ")
@@ -29,17 +31,15 @@ while typ_boh.upper() not in ['A', 'D', 'Z']:
         print("Nie wybrałes typu bohatera !!!!!! spróboj ponownie")
 
 # atak podstawowy
-
 def normalny_atak():
     if typ_boh.upper() in ['Z', 'D']:
         return randint(2,8)
     else:
         return randint(3, 15)
-# atak specialny
 
+# atak specjalny
 def specjalny_atak():
     if typ_boh.upper() == 'Z':
-        global kasa_boh
         kasa_boh = kasa_boh - 30
         return randint(5,15)
     elif typ_boh.upper() == 'D':
@@ -49,10 +49,10 @@ def specjalny_atak():
     else:
         kasa_boh = kasa_boh - 20
         return randint(10,20)
-# wybieranie ataku
 
+# wybieranie ataku
 def wybieranie_ataku():
-    print("Wybierz atak: ")
+    print("\n Wybierz atak: ")
     if typ_boh.upper() == "Z":
         print("Atak podstawowy(2-8 hp) = p/P")
         print('Super atak(5-15 hp) = s/S ')
@@ -105,36 +105,58 @@ def wybieranie_ataku():
         else:
             print("Nie wybrałes ataku! ")
             return 0
-# Przeciwnicy
-goblin = ["Mały Goblin",20,3,0]
-nymph = ["Nimfa Wodna",15,3,0]
-list_Opponents = [goblin,nymph]
+
+# Przeciwnicy 1 poziom
+# 0 - imie, 1 - hp, 2 - atak, 3 - super atak
+prze1 = ["Elf", 10, 50, 0]
+prze2 = ["Gnom", 5, 40, 0]
+prze3 = ["Cyklop", 15, 20, 0]
+lista_prze = [prze1, prze2, prze3]
+
+def random_prze():
+    przeciwnik = lista_prze[randint(0,2)].copy()
+    return przeciwnik
+
 
 # gra
-def random_oponent():
-    Opponent =  list_Opponents[randint(0,1)].copy()
-    return Opponent
 liczba_pokonanych_przeciwników = 0
 while hp_boh > 0:
-    Opponent = random_oponent()
-    print("-"*40)
+    Opponent = random_prze()
+    print("="*40)
     while Opponent[1] > 0 :
         print(f"{imie_boh} walczy teraz z {Opponent[0]}")
         print(f"Przeciwnik ma {Opponent[1]} Hp i zadaje ci {Opponent[2]} obrażeń")
         hp_boh = hp_boh - Opponent[2]
-        if hp_boh < 0:
+        if hp_boh <= 0:
             break
         print(f"Zostało ci {hp_boh} Hp i {kasa_boh} Many")
         atak  = wybieranie_ataku()
         Opponent[1] = Opponent[1] - atak
-        print(f"Zadałeś {atak} obrażeń")
-    print("-"*40)
-    print('Zabiłeś przeciwnika !!!')
-    liczba_pokonanych_przeciwników = liczba_pokonanych_przeciwników + 1
+        print(f"Zadałeś {atak} obrażeń \n")
+    if hp_boh > 0:
+        print("-"*40)
+        print('Zabiłeś przeciwnika !!!')
+        liczba_pokonanych_przeciwników = liczba_pokonanych_przeciwników + 1
+    
 
-print("-"*40)
-print("YOU LOST !!!!")
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("="*40)
+print("Zginąłes !!!!")
 print(f"Zabiles {liczba_pokonanych_przeciwników} przeciwników")
+print("\n KONIEC GRY")
     
         
     
